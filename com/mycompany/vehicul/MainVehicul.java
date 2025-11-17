2e4b9000f15fb1677bd88ba55c4025e1aa5cd450
AlexandraNiculescu-jpg <an393@student.ugal.ro>
2025-11-17 09:19:42 +0200

    Added instances of Trotineta in MainVehicul
    Added instances of Trotineta and tested their methods.

--- com/mycompany/vehicul/MainVehicul.java
+++ com/mycompany/vehicul/MainVehicul.java
@@ -18,7 +18,22 @@ public class MainVehicul {
         Masina m1, m2, m3;
         Electrica e1, e2, e3;
         Trotineta t1, t2, t3;
+        rotineta t1 = new Trotineta();
+        Trotineta t2 = new Trotineta("Xiaomi", 1500.5f, 80, (byte) 60);
+        Trotineta t3 = new Trotineta(t2);
+
+        System.out.println("Trotineta 1: " + t1);
+        System.out.println("Trotineta 2: " + t2);
+        System.out.println("Trotineta 3: " + t3);
         
+        System.out.println();
+        System.out.println("Test reincarcare:");
+        t1.reincarca();
+        t2.reincarca();
+        t3.reincarca();
+    }
+}
+
         v1=new Vehicul();
         v2=new Vehicul("Peugeot",3000.99f);
         v3=new Vehicul(v2);
